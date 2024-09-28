83e5722258bfe0030d2da2394f6ec1333a2bc3c3
achyudtv <achyudt@umich.edu>
2024-09-28 13:45:03 -0400

    Initial Commit

---
+++ SCANSCanscan/SCANSCanscanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCANSCanscanApp.swift
+//  SCANSCanscan
+//
+//  Created by Achyudt Venkat on 9/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SCANSCanscanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SCANSCanscanTests/SCANSCanscanTests.swift
@@ -0,0 +1,17 @@
+//
+//  SCANSCanscanTests.swift
+//  SCANSCanscanTests
+//
+//  Created by Achyudt Venkat on 9/28/24.
+//
+
+import Testing
+@testable import SCANSCanscan
+
+struct SCANSCanscanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
